a48aa72478a0d710ed7e9271954e97d938288253
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-01-07 11:26:29 -0600

    Add operator function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const OPERATOR = {
+  ADD: "ADD",
+  SUBTRACT: "SUBTRACT",
+  MULTIPLY: "MULTIPLY",
+  DIVIDE: "DIVIDE",
+};
+
 function add(...numbers) {
   return numbers.reduce((acc, number) => acc + number, 0);
 }
@@ -14,4 +21,17 @@ function multiply(...numbers) {
 
 function divide(...numbers) {
   return numbers.reduce((acc, number) => acc / number);
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case OPERATOR.ADD:
+      return add(num1, num2);
+    case OPERATOR.SUBTRACT:
+      return subtract(num1, num2);
+    case OPERATOR.MULTIPLY:
+      return multiply(num1, num2);
+    case OPERATOR.DIVIDE:
+      return divide(num1, num2);
+  }
 }
